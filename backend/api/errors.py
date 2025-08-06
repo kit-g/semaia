75e64fc7e84cc517524d5bd2d348d571a045b58b
_kitg <gerasimovkit@gmail.com>
2025-08-06 12:50:45 -0400

    feat: error types

---
+++ backend/api/errors.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from typing import Any
+
+
+class EmptyResponse(Exception):
+    pass
+
+
+@dataclass
+class IncorrectSignature(Exception):
+    required: list[str] = None
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            'required': self.required,
+        }
+
+
+class Unauthorized(Exception):
+    pass
+
+
+@dataclass
+class NotFound(Exception):
+    path: str = None
+
+    def __str__(self):
+        return f'Not found: {self.path}'
